f5bea5edd952b1090393dad72a6bed0b8b815ac7
Oksana <oksanabibik93@gmail.com>
2019-06-06 16:42:42 -0700

    initial setup

---
+++ frontend/pages/index.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+// import React from 'react';   // next js will take care of it
+
+// !! Class component
+// class Home extends React.Component {
+//     render() {
+//         return <p>Hello!</p>;
+//     }
+// }
+
+// We are replacing the commented section above with the following below:
+// !! stateless Functional component
+const Home = props => (
+    <div>
+        <p>Hello World!</p>
+    </div>
+);
+
+export default Home;
