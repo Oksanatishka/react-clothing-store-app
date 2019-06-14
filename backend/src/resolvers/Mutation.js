14bd3b6c375491dad514be3aa486dd0c527111df
Oksana <oksanabibik93@gmail.com>
2019-06-14 15:57:54 -0700

    add queries and mutations

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,26 @@
-const Mutations = {};
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO: Check if they are logged in
+        // ctx.db.mutation.createItem   - that actually returns a promise,
+        const item = await ctx.db.mutation.createItem(
+            {
+                data: {
+                    ...args
+                }
+            },
+            info
+        );
+
+        return item;
+    }
+    // createDog(parent, args, ctx, info) {
+    //     global.dogs = global.dogs || [];
+    //     // create dog
+    //     const newDog = { name: args.name };
+    //     global.dogs.push(newDog);
+    //     return newDog;
+    //     // console.log(args);
+    // }
+};
 
 module.exports = Mutations;
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,22 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    items: forwardTo('db')
+    // this was going to return a promise
+    // items(parent, args, ctx, info) {
+    //     return ctx.db.query.items();
+    // }
+
+    // async items(parent, args, ctx, info) {
+    //     const items = await ctx.db.query.items();
+    //     return items;
+    // }
+
+    // dogs(parent, args, ctx, info) {
+    //     // return [{ name: 'Snickers' }, { name: 'Sunny' }];
+    //     global.dogs = global.dogs || [];
+    //     return global.dogs;
+    // }
+};
 
 module.exports = Query;
