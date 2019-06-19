4ab9cf11debb2587321e8e32bb3718ba9bbec643
Oksana <oksanabibik93@gmail.com>
2019-06-18 17:15:37 -0700

    uploading images

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -41,6 +41,26 @@ class CreateItem extends Component {
         // this.setState({ title: e.target.value });
         this.setState({ [name]: val });
     };
+
+    uploadFile = async e => {
+        console.log('Uploading File...');
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits'); // taken from Settings in Cloudinary
+
+        const res = await fetch('https://api.cloudinary.com/v1_1/dizsd92ok/image/upload', {
+            method: 'POST',
+            body: data
+        });
+        const file = await res.json();
+        console.log(file);
+        this.setState({
+            image: file.secure_url
+            // largeImage: file.eager[0].secure_url
+        });
+    };
+
     render() {
         return (
             <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -65,6 +85,22 @@ class CreateItem extends Component {
                         <Error error={error} />
                         {/* <h2>Sell an Item.</h2> */}
                         <fieldset disabled={loading} aria-busy={loading}>
+                            <label htmlFor="file">
+                                Image
+                                <input
+                                    type="file"
+                                    id="file"
+                                    name="file"
+                                    placeholder="Upload an image"
+                                    required
+                                    // value={this.state.image}
+                                    onChange={this.uploadFile}
+                                />
+                                {this.state.image && (
+                                    <img src={this.state.image} width="200" alt="Upload Preview" />
+                                )}
+                            </label>
+
                             <label htmlFor="title">
                                 Title
                                 <input
@@ -113,3 +149,4 @@ class CreateItem extends Component {
 }
 
 export default CreateItem;
+export { CREATE_ITEM_MUTATION };
