f7ae2feca472ed1fbd517822d6c91ebace9a5cc8
Oksana <oksanabibik93@gmail.com>
2019-10-03 22:20:46 -0700

    add Accounts, Authentication and Permissions

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,24 +1,34 @@
 const { forwardTo } = require('prisma-binding');
+const { hasPermission } = require('../utils');
 
 const Query = {
     items: forwardTo('db'),
     item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
-    // this was going to return a promise
-    // items(parent, args, ctx, info) {
-    //     return ctx.db.query.items();
-    // }
+    itemsConnection: forwardTo('db'),
+    me(parent, args, ctx, info) {
+        // check if there is a current user ID
+        if (!ctx.request.userId) {
+            return null;
+        }
+        return ctx.db.query.user(
+            {
+                where: { id: ctx.request.userId }
+            },
+            info
+        );
+    },
+    async users(parent, args, ctx, info) {
+        // 1. Check if they are logged in
+        if (!ctx.request.userId) {
+            throw new Error('You must be logged in!');
+        }
+        console.log(ctx.request.userId);
+        // 2. Check if the user has the permissions to query all the users
+        hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE']);
 
-    // async items(parent, args, ctx, info) {
-    //     const items = await ctx.db.query.items();
-    //     return items;
-    // }
-
-    // dogs(parent, args, ctx, info) {
-    //     // return [{ name: 'Snickers' }, { name: 'Sunny' }];
-    //     global.dogs = global.dogs || [];
-    //     return global.dogs;
-    // }
+        // 2. if they do, query all the users!
+        return ctx.db.query.users({}, info);
+    }
 };
 
 module.exports = Query;
